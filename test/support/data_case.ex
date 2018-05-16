a52d15899963a60ebaf6a6c6a96b02371fff5ef4
Zacck Osiemo <coderv63@gmail.com>
2018-05-16 11:55:11 +0200

    Add person factory and modify datacase to use factory

--- test/support/data_case.ex
+++ test/support/data_case.ex
@@ -22,6 +22,7 @@ defmodule Contractor.DataCase do
       import Ecto.Changeset
       import Ecto.Query
       import Contractor.DataCase
+      import Contractor.Factory
     end
   end
 
--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,5 +1,11 @@
 defmodule Contractor.Factory do
   use ExMachina.Ecto, repo: Contractor.Repo
 
-
+  def person_factory do
+    %Contractor.Accounts.Person{
+      email: sequence(:email, &"user-#{&1}@contractor.com"),
+      token: sequence(:token, &"user-#{&1}-token"),
+      hash: sequence(:token, &"user-#{&1}-hash")
+    }
+  end
 end
