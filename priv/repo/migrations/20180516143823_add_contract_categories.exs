b27f73cce89aecb34c5b86ae8038ccef10e5bd19
Zacck Osiemo <coderv63@gmail.com>
2018-05-16 17:15:59 +0200

    Add migrations for categories

---
+++ priv/repo/migrations/20180516143823_add_contract_categories.exs
@@ -0,0 +1,13 @@
+defmodule Contractor.Repo.Migrations.AddContractCategories do
+  use Ecto.Migration
+
+  def change do
+    create table(:categories, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :name, :string
+
+      timestamps(inserted_at: :created_at, updated_at: :updated_at)
+      add :vendor_id, references(:vendors, on_delete: :nothing, type: :uuid)
+    end
+  end
+end
