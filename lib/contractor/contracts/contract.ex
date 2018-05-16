9e77c066babbae6a2dc5ec2d073b3e93aa709116
Zacck Osiemo <coderv63@gmail.com>
2018-05-16 20:50:06 +0200

    Test drive contract changesets

--- lib/contractor/contracts/contract.ex
+++ lib/contractor/contracts/contract.ex
@@ -23,4 +23,14 @@ defmodule Contractor.Contracts.Contract do
     belongs_to :vendor, Vendor, foreign_key: :vendor_id, type: :binary_id
     belongs_to :category, Category, foreign_key: :category_id, type: :binary_id
   end
+
+  @spec create_changeset(Person.t, Vendor.t, Category.t, map) :: Ecto.Changeset.t()
+  def create_changeset(%Person{} = person, %Vendor{} = vendor, %Category{} = category, attrs) do
+    %Contract{}
+    |> cast(attrs,[:cost, :end_date])
+    |> validate_required([:cost, :end_date])
+    |> put_assoc(:person, person)
+    |> put_assoc(:vendor, vendor)
+    |> put_assoc(:category, category)
+  end
 end
--- test/lib/contractor/contracts/contract_test.exs
+++ test/lib/contractor/contracts/contract_test.exs
@@ -5,5 +5,27 @@ defmodule Contractor.Contracts.ContractTest do
     Contracts.Contract
   }
 
-  
+  @valid_attrs %{cost: 90.89, end_date: "2018-12-12"}
+  describe "contract changesets" do
+    test "valid with correct data" do
+      person = insert(:person)
+      vendor = insert(:vendor)
+      category = insert(:category, vendor: vendor)
+
+      assert changeset = Contract.create_changeset(person, vendor, category, @valid_attrs)
+      assert changeset.changes.person
+      assert changeset.changes.vendor
+      assert changeset.changes.category
+      assert changeset.valid?
+    end
+
+    test "invalid when required fields are missing" do
+      person = insert(:person)
+      vendor = insert(:vendor)
+      category = insert(:category, vendor: vendor)
+      assert changeset = Contract.create_changeset(person, vendor, category, %{})
+      refute changeset.valid?
+    end
+  end
+
 end
