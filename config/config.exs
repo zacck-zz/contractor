74882858423c8632da8880bdf8d5d11ffef80291
Zacck Osiemo <coderv63@gmail.com>
2018-05-16 14:22:53 +0200

    Add and configure ueberauth and Guardian

--- config/config.exs
+++ config/config.exs
@@ -22,6 +22,20 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+
+# Configures Ueberauth
+config :ueberauth, Ueberauth,
+  providers: [
+    identity: {Ueberauth.Strategy.Identity. [
+      callback_methods: ["POST"]
+    ]}
+  ]
+
+# Configures Guardian
+config :contractor, Contractor.Auth.Guardian,
+  issuer: "VoldersContractor",
+  secret_key: System.get_env("MIX_SECRET")
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
