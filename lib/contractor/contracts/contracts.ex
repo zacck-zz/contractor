2b1b35ddb0a42991b862ac506613f54e9a507cc4
Zacck Osiemo <coderv63@gmail.com>
2018-05-17 08:21:43 +0200

    Add and test functionality to update and delete contracts

--- lib/contractor/contracts/contracts.ex
+++ lib/contractor/contracts/contracts.ex
@@ -1,9 +1,39 @@
 defmodule Contractor.Contracts do
   alias Contractor.{
     Repo,
+    Accounts.Person,
     Contracts.Category,
+    Contracts.Contract,
     Contracts.Vendor
   }
+
+  @spec add_contract(Person.t, Vendor.t, Category.t, map) :: {:ok, Contract.t} | {:error, Ecto.Changeset.t()}
+  def add_contract(%Person{} = person, %Vendor{} = vendor, %Category{} = category, attrs) do
+    Contract.create_changeset(person, vendor, category, attrs)
+    |> Repo.insert()
+  end
+
+  @spec update_contract(Contract.t, Vendor.t, Category.t, map) :: {:ok, Contract.t} | {:error, Ecto.Changeset.t()}
+  def update_contract(%Contract{} = contract, %Vendor{} = vendor, %Category{} = category, attrs) do
+    contract = contract |> Repo.preload([:person, :vendor, :category])
+
+    contract
+    |> Contract.update_changeset(vendor, category, attrs)
+    |> Repo.update()
+  end
+
+  @spec delete_contract(Contract.t) :: {:ok, Contract.t} | {:error, Ecto.Changeset.t()}
+  def delete_contract(%Contract{} = contract) do
+    contract
+    |> Repo.delete()
+  end
+
+  @spec add_category(Vendor.t, map) :: {:ok, Category.t} | {:error, Ecto.Changeset.t()}
+  def add_category(%Vendor{} = vendor, attrs) do
+    vendor
+    |> Category.create_changeset(attrs)
+    |> Repo.insert()
+  end
 
   @spec get_vendors() :: {:ok, list(Vendor.t)} | {:error, String.t()}
   def get_vendors() do
--- test/lib/contractor/contracts/contracts_test.exs
+++ test/lib/contractor/contracts/contracts_test.exs
@@ -4,12 +4,63 @@ defmodule Contractor.ContractsTest do
   alias Contractor.{
     Contracts,
     Contracts.Category,
+    Contracts.Contract,
     Contracts.Vendor
   }
 
   @num 25
   @valid_vendor %{name: "Orange"}
+  @valid_category %{name: "Phone"}
+  @valid_contract %{cost: 90.89, end_date: "2018-12-12"}
   describe "Contracts boundary " do
+
+    test "can add a user contract" do
+      person = insert(:person)
+      vendor = insert(:vendor)
+      category = insert(:category, vendor: vendor)
+      assert Repo.aggregate(Contract, :count, :id) == 0
+      assert {:ok, _} = Contracts.add_contract(person, vendor, category, @valid_contract)
+      assert Repo.aggregate(Contract, :count, :id) == 1
+    end
+
+    test "can update a user contract" do
+      contract = insert(:contract)
+      assert Repo.aggregate(Contract, :count, :id) == 1
+      vendor = insert(:vendor)
+      category = insert(:category, vendor: vendor)
+      assert {:ok, updated_contract} = Contracts.update_contract(contract, vendor, category, @valid_contract)
+      assert updated_contract.id == contract.id
+      refute updated_contract.cost == contract.cost
+      refute updated_contract.end_date == contract.end_date
+      assert Repo.aggregate(Contract, :count, :id) == 1
+      assert updated_contract.cost == @valid_contract.cost
+      assert Date.to_string(updated_contract.end_date) == @valid_contract.end_date
+    end
+
+    test "can delete a user contract" do
+      contract = insert(:contract)
+      assert Repo.aggregate(Contract, :count, :id) == 1
+      {:ok, deleted_contract} = Contracts.delete_contract(contract)
+      assert Repo.aggregate(Contract, :count, :id) == 0
+      assert deleted_contract.id == contract.id
+    end
+
+    test "can add a category" do
+      vendor = insert(:vendor)
+      assert Repo.aggregate(Vendor, :count, :id) == 1
+      assert Repo.aggregate(Category, :count, :id) == 0
+      assert {:ok, _} = Contracts.add_category(vendor, @valid_category)
+      assert Repo.aggregate(Category, :count, :id) == 1
+    end
+
+    test "errors out if category details are invalid" do
+      vendor = insert(:vendor)
+      assert Repo.aggregate(Vendor, :count, :id) == 1
+      assert Repo.aggregate(Category, :count, :id) == 0
+      assert {:error, _} = Contracts.add_category(vendor, %{})
+      assert Repo.aggregate(Category, :count, :id) == 0
+    end
+
     test "list all available vendors" do
       insert_list(@num, :vendor)
       assert {:ok, vendors} = Contracts.get_vendors()
