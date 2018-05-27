0609094d5bab4631e28a9a3d87f573f7132f3b93
Zacck Osiemo <coderv63@gmail.com>
2018-05-27 22:49:06 +0200

    split state, utils and types in app

--- assets/elm/Main.elm
+++ assets/elm/Main.elm
@@ -2,9 +2,9 @@ module Main exposing (..)
 
 import Html exposing (Html, div, text)
 import Http
-import Json.Encode as Encode
-import Json.Decode as Decode exposing (..)
-
+import Types exposing (Model, Msg(..))
+import State exposing (peopleQuery, update)
+import Utils exposing (authedGraphRequest)
 
 main =
   Html.program
@@ -15,77 +15,9 @@ main =
       }
 
 
-type alias Model =
-  { response : String
-  }
-
-
-type Msg
-    = FetchPeople (Result Http.Error String)
-
-
-graphUrl : String
-graphUrl =
-    "http://localhost:4000/api/graph"
-
-
--- Draw up a people query
-peopleQuery : String
-peopleQuery =
-    """
-    {
-      people {
-        id
-        name
-      }
-    }
-    """
-
--- Encode query object
-queryEncoder : String -> Encode.Value
-queryEncoder query =
-    Encode.object
-      [("query", Encode.string query)]
-
-
--- Decode Http String Result
-resultDecoder : Decoder String
-resultDecoder =
-    Decode.field "data" Decode.string
-
--- build a Request with our query
-graphRequest : String -> String -> Http.Request String
-graphRequest url data =
-  let
-    body =
-        data
-          |> queryEncoder
-          |> Http.jsonBody
-          |> Debug.log("data")
-
-
-  in
-      Http.post url body resultDecoder
-
-
-
-
 init : (Model, Cmd Msg)
 init = { response = "Waiting For a response ... "} ! [
- Http.send FetchPeople (graphRequest graphUrl peopleQuery) ]
-
-
-
-update : Msg -> Model -> (Model, Cmd Msg)
-update msg model =
-   case msg of
-     FetchPeople response ->
-       case response of
-         Ok result ->
-           { response = result } ! []
-
-         Err err ->
-           { response = toString err } ! []
+ Http.send FetchPeople (authedGraphRequest "" peopleQuery) ]
 
 view : Model -> Html Msg
 view model =
