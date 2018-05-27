51863982769750f8b7ee796bf9e5917262dbf2f4
Zacck Osiemo <coderv63@gmail.com>
2018-05-28 00:11:23 +0200

    add person and update model types

--- assets/elm/Types.elm
+++ assets/elm/Types.elm
@@ -4,9 +4,16 @@ import Http
 
 type alias Model = 
     { response : String
+    , token : String 
     }
 
 
 type Msg
     = FetchPeople (Result Http.Error String)
 
+
+type alias Person =  
+    { id : String
+    , name : String 
+    , email : String 
+    }
