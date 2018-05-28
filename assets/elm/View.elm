f866a294cdac5727c23f62b70ae46b262ce49e3c
Zacck Osiemo <coderv63@gmail.com>
2018-05-28 17:02:40 +0200

    build sign up form handle validation and add submit message

--- assets/elm/View.elm
+++ assets/elm/View.elm
@@ -1,11 +1,14 @@
 module View exposing (..)
 
-import Html exposing(Html, text, div, a, button)
+import Html exposing(Html, text, div, a, button, input, label, li, ul)
 import Types exposing(Model, Msg(..), Page(..))
-import Html.Events exposing(onClick)
+import Html.Events exposing(onClick, onInput)
 import Html.Attributes exposing(..)
 import State exposing(setRoute)
 import Route
+
+
+
 
 view : Model -> Html Msg
 view model =
@@ -17,13 +20,13 @@ view model =
             div [] [ text "Contracts" ]
 
         ContractDetails ->
-            div [] [ text "COntractDetails" ]
+            div [] [ text "ContractDetails" ]
 
         SignIn ->
             div [] [ text "Login Page" ]
 
         SignUp ->
-            div [] [ text "Sign Up Page" ]
+            div [] [ (signUpView model) ]
 
         AddContract ->
             div [] [ text "Add Contract" ]
@@ -52,3 +55,36 @@ homeView model =
                 [ a [ onClick <| NavigateTo Route.Contracts ]
                     [ text "Go to Contracts"]
                 ]
+
+
+signUpView : Model -> Html Msg
+signUpView model =
+    div [ class "app-box" ]
+        [ div [ class "home-box" ]
+              [ div [] [(signUpErrors model.errors)]
+              , label [ class "label"]
+                [ text "Name"
+                , input [ type_ "text", placeholder "Type your name", onInput SetName, value model.name ] []
+                ]
+              , label [ class "label"]
+                [ text "Email"
+                , input [ type_ "text", placeholder "Your email address", onInput SetEmail, value model.email ] []
+                ]
+              , label [ class "label"]
+                [ text "Password"
+                , input [ type_ "password", onInput SetPassword, value model.password ] []
+                ]
+              , label [ class "label"]
+                [ text "Password Confirmation"
+                , input [ type_ "password", onInput SetPasswordConf, value model.passwordconf] []
+                ]
+              , button [ onClick SubmitSignUp ] [ text "SIGN UP"]
+              ]
+        ]
+
+
+signUpErrors : List String -> Html msg
+signUpErrors errors =
+  errors
+      |> List.map(\error -> li [] [ text error])
+      |> ul []
