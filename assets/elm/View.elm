c2c0408f3a73c3912052c0cecc06905cc849d483
Zacck Osiemo <coderv63@gmail.com>
2018-05-28 11:13:51 +0200

    add html views for all pages

--- assets/elm/View.elm
+++ assets/elm/View.elm
@@ -1,9 +1,31 @@
 module View exposing (..)
 
 import Html exposing(Html, text, div)
-import Types exposing(Model, Msg(..))
+import Types exposing(Model, Msg(..), Page(..))
 
 
 view : Model -> Html Msg
 view model =
-    div [] [ text model.response ]
+    case model.page of 
+        Home ->
+            div [] [ text model.response ]
+        
+        Contracts ->
+            div [] [ text "Contracts" ]
+        
+        ContractDetails ->
+            div [] [ text "COntractDetails" ]
+        
+        SignIn -> 
+            div [] [ text "Login Page" ]
+        
+        SignUp ->
+            div [] [ text "Sign Up Page" ]
+        
+        AddContract ->
+            div [] [ text "Add Contract" ]
+        
+        UpdateContract ->
+            div [] [ text "Update Contract" ]
+
+
