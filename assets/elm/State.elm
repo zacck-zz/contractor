e8c1c473b8c91603cb515b0fba8d9b0b19fe17b7
Zacck Osiemo <coderv63@gmail.com>
2018-05-28 11:10:41 +0200

    add routing logic and state

--- assets/elm/State.elm
+++ assets/elm/State.elm
@@ -4,8 +4,11 @@ module State exposing(..)
 import Http 
 import Json.Encode as Encode 
 import Json.Decode as Decode 
-import Types exposing (Model, Msg(..))
+import Types exposing (Model, Msg(..), Page(..))
 import Utils exposing (graphUrl, queryBody, authedGraphRequest)
+import Navigation exposing (Location)
+import UrlParser
+import Route
 
 -- Draw up a people query
 peopleQuery : String
@@ -19,7 +22,29 @@ peopleQuery =
     }
     """
 
-
+-- parsePath reads the path in the url and turns it into a new type
+setRoute : Location -> Model -> Model 
+setRoute location model =
+    let
+       route = 
+        UrlParser.parsePath Route.route location
+           |> Maybe.withDefault Route.Home
+    in   
+        case route of 
+            Route.Home ->
+                { model | page = Home }
+            Route.Contracts ->
+                { model | page = Contracts }
+            Route.ContractDetails ->
+                { model | page = ContractDetails }
+            Route.SignIn ->
+                { model | page = SignIn }
+            Route.SignUp ->
+                { model | page = SignUp }
+            Route.AddContract ->
+                { model | page = AddContract }
+            Route.UpdateContract -> 
+                { model | page = UpdateContract } 
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
@@ -31,6 +56,8 @@ update msg model =
 
          Err err ->
            ({ model | response = toString err }, Cmd.none)
+     SetRoute location -> 
+         ( setRoute location model, Cmd.none )
 
 
 
