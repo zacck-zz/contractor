e6600286678b97b87b32de3dbd4d653cea7b0ead
Zacck Osiemo <coderv63@gmail.com>
2018-05-28 11:11:24 +0200

    add routing message and page types

--- assets/elm/Types.elm
+++ assets/elm/Types.elm
@@ -1,15 +1,29 @@
 
 module Types exposing (..)
 import Http
+import Navigation exposing (Location)
 
 type alias Model = 
     { response : String
-    , token : String 
+    , token : String
+    , page : Page  
     }
+
+
+type Page 
+   = Home 
+   | Contracts 
+   | ContractDetails 
+   | SignIn
+   | SignUp
+   | AddContract 
+   | UpdateContract 
+
 
 
 type Msg
     = FetchPeople (Result Http.Error String)
+    | SetRoute Location 
 
 
 type alias Person =  
