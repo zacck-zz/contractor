b160f156f48632dc9b81762bc4fe035fcb1f90ec
Zacck Osiemo <coderv63@gmail.com>
2018-05-28 11:08:48 +0200

    send all paths from phoenix to elm

--- lib/contractor_web/router.ex
+++ lib/contractor_web/router.ex
@@ -24,7 +24,7 @@ defmodule ContractorWeb.Router do
   scope "/", ContractorWeb do
     pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
+    get "/*path", PageController, :index
   end
 
   # Other scopes may use custom stacks.
