0ce784c650a2732b120166d5c3a300e947a65484
Zacck Osiemo <coderv63@gmail.com>
2018-06-14 03:19:30 +0200

    Add an eager implementation of a computationaly expensive event

--- lib/producer_consumer.ex
+++ lib/producer_consumer.ex
@@ -7,4 +7,23 @@ stage and an output stage
 
 
 
+ @doc """
+ This function reads this file in an eager way
+ iex> Contractor.Stage.consume(:eager, "path")
+ %{word => count}
+ """
+ @spec consume(atom(), String.t) :: map()
+  def consume(:eager, path) do
+    {:ok, file} = File.read(path) #load file into memory
+
+    #split the file into lines
+    map =
+      file
+      |> String.split("\n") # split file into lines
+      |> Enum.flat_map(&String.split/1) # for each item in this list of lines to words
+      |> Enum.reduce(%{}, fn word, map ->
+         Map.update(map, word, 1, & &1 + 1)
+       end)
+  end
+
 end
