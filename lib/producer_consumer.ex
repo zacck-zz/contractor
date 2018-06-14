03fa80ca30531d3ea0a166c788fc736fbaefa52b
Zacck Osiemo <coderv63@gmail.com>
2018-06-14 03:17:48 +0200

    Add a Stage module for experiments

---
+++ lib/producer_consumer.ex
@@ -0,0 +1,10 @@
+defmodule Contractor.Stage do
+@moduledoc """
+Module that contains the stages of the application
+At the moment the plan is to leverage 3 stages an input, a processing
+stage and an output stage
+"""
+
+
+
+end
